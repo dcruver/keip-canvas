aef48448e33a2edad25bff9c8673675065fe4f47
Abdullah Asaad <abdullah.asaad@connexta.com>
2024-07-23 16:12:56 -0700

    Flow translator webapp initial check in

---
+++ flow-translator/flow-translator-webapp/src/main/java/com/octo/keip/flow/web/FlowTranslatorWebApplication.java
@@ -0,0 +1,13 @@
+package com.octo.keip.flow.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowTranslatorWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowTranslatorWebApplication.class, args);
+	}
+
+}
---
+++ flow-translator/flow-translator-webapp/src/test/java/com/octo/keip/flow/web/FlowTranslatorWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octo.keip.flow.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowTranslatorWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
