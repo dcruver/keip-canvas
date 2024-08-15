f6f39cc4a8f501f2b312307e56288ae71f67b60e
Abdullah Asaad <abdullah.asaad@connexta.com>
2024-08-15 14:34:55 -0700

    Remove Export to XML header menu option

--- ui/src/components/options-menu/OptionsMenu.tsx
+++ ui/src/components/options-menu/OptionsMenu.tsx
@@ -1,8 +1,6 @@
-// @ts-expect-error: OverflowMenu is not expoerted properly from Carbon React
 import { OverflowMenu } from "@carbon/react"
 import { Menu } from "@carbon/react/icons"
 import ExportPng from "./ExportPng"
-import ExportXml from "./ExportXml"
 import SaveDiagram from "./SaveDiagram"
 
 const OptionsMenu = () => {
@@ -20,7 +18,6 @@ const OptionsMenu = () => {
       {/* OverflowMenu does not play nice when OverflowMenuItems are not direct children. Custom components will need to use forwardRef to avoid errors. */}
       <SaveDiagram />
       <ExportPng />
-      <ExportXml />
     </OverflowMenu>
   )
 }
