c362ba496d69a5274d89f58fd2c543b9c6219e5b
Abdullah Asaad <abdullah.asaad@connexta.com>
2024-08-28 18:56:31 -0700

    Log exceptions caught by global handler

--- flow-translator/flow-translator-webapp/src/main/java/com/octo/keip/flow/web/error/ApiError.java
+++ flow-translator/flow-translator-webapp/src/main/java/com/octo/keip/flow/web/error/ApiError.java
@@ -2,18 +2,16 @@ package com.octo.keip.flow.web.error;
 
 import java.util.Collections;
 import java.util.List;
+import org.springframework.core.NestedExceptionUtils;
 
 public record ApiError<T>(String message, String type, List<T> details) {
 
   public static <T> ApiError<T> of(Exception ex) {
-    return new ApiError<>(ex.getMessage(), getCause(ex), Collections.emptyList());
+    return of(ex, Collections.emptyList());
   }
 
   public static <T> ApiError<T> of(Exception ex, List<T> details) {
-    return new ApiError<>(ex.getMessage(), getCause(ex), details);
-  }
-
-  private static String getCause(Exception ex) {
-    return ex.getCause() == null ? "INTERNAL" : ex.getCause().getClass().getSimpleName();
+    Throwable root = NestedExceptionUtils.getMostSpecificCause(ex);
+    return new ApiError<>(root.getMessage(), root.getClass().getSimpleName(), details);
   }
 }
