f340f3e4158713638ace52f2325b333dde265350
Abdullah Asaad <abdullah.asaad@connexta.com>
2024-09-04 12:07:21 -0700

    Validate model enums

--- flow-translator/flow-translator-lib/src/test/groovy/com/octo/keip/flow/model/FlowModelValidationTest.groovy
+++ flow-translator/flow-translator-lib/src/test/groovy/com/octo/keip/flow/model/FlowModelValidationTest.groovy
@@ -1,6 +1,6 @@
 package com.octo.keip.flow.model
 
-
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.cfg.EnumFeature
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.octo.keip.schemas.validation.EipSchema
@@ -13,10 +13,7 @@ import spock.lang.Specification
  */
 class FlowModelValidationTest extends Specification {
 
-    private static final JsonMapper MAPPER =
-            JsonMapper.builder()
-                      .enable(EnumFeature.WRITE_ENUMS_TO_LOWERCASE)
-                      .build();
+    private static final JsonMapper MAPPER = createMapper()
 
     def validator = EipSchemaValidator.getInstance(EipSchema.FLOW)
 
@@ -40,5 +37,78 @@ class FlowModelValidationTest extends Specification {
         def errors = validator.validate(new StringReader(flowStr))
         then:
         errors.isEmpty()
+    }
+
+    def "Validate ConnectionType Enum values"(ConnectionType connectionType) {
+        given:
+        def node = new EipNode("n1",
+                new EipId("test-ns", "test-comp"),
+                null,
+                null,
+                Role.TRANSFORMER,
+                connectionType,
+                null,
+                null)
+
+        def flow = new Flow([node], [])
+        when:
+        def flowStr = MAPPER.writeValueAsString(flow)
+        def errors = validator.validate(new StringReader(flowStr))
+        then:
+        errors.isEmpty()
+        where:
+        connectionType << ConnectionType.values()
+    }
+
+    def "Validate Role Enum values"(Role role) {
+        given:
+        def node = new EipNode("n1",
+                new EipId("test-ns", "test-comp"),
+                null,
+                null,
+                role,
+                ConnectionType.REQUEST_REPLY,
+                null,
+                null)
+
+        def flow = new Flow([node], [])
+        when:
+        def flowStr = MAPPER.writeValueAsString(flow)
+        def errors = validator.validate(new StringReader(flowStr))
+        then:
+        errors.isEmpty()
+        where:
+        role << Role.values()
+    }
+
+    def "Validate EdgeType Enum values"(EdgeProps.EdgeType edgeType) {
+        given:
+        def node = new EipNode("n1",
+                new EipId("test-ns", "test-comp"),
+                null,
+                null,
+                Role.ENDPOINT,
+                ConnectionType.REQUEST_REPLY,
+                null,
+                null)
+
+        def edge = new FlowEdge("e1", "n1", "n2", edgeType)
+
+        def flow = new Flow([node], [edge])
+        when:
+        def flowStr = MAPPER.writeValueAsString(flow)
+        def errors = validator.validate(new StringReader(flowStr))
+        then:
+        errors.isEmpty()
+        where:
+        edgeType << EdgeProps.EdgeType.values()
+    }
+
+    private static JsonMapper createMapper() {
+        JsonMapper mapper = JsonMapper.builder()
+                                      .enable(EnumFeature.WRITE_ENUMS_TO_LOWERCASE)
+                                      .build()
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        return mapper
     }
 }
